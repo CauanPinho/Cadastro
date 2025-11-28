3e9579a1d63b26dccfd7a0a559a35cfafd828bb5
Cauan Pinho <Jobscauanpinho@gmail.com>
2025-11-28 10:38:40 -0400

    implementação dos cases

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,77 +1,99 @@
 import entities.Product;
-
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Application {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
         ArrayList<Product> products = new ArrayList<>();
 
+        boolean running = true;
 
-    boolean running = true;
+        while (running) {
+
+            System.out.println(
+                    "===== MENU =====\n" +
+                            "1 - Cadastrar produto\n" +
+                            "2 - Listar produtos\n" +
+                            "3 - Atualizar quantidade\n" +
+                            "4 - Atualizar preço\n" +
+                            "5 - Remover produto\n" +
+                            "6 - Buscar produto\n" +
+                            "7 - Sair\n"
+            );
+
+            System.out.print("Escolha uma opção: ");
+            int option = sc.nextInt();
+            sc.nextLine(); // limpa ENTER
+
+            switch (option) {
+
+                case 1:
+                    System.out.println("Digite o nome do produto:");
+                    String nome = sc.nextLine();
+
+                    System.out.println("Digite a quantidade:");
+                    int quantidade = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.println("Digite o preço:");
+                    double preco = sc.nextDouble();
+                    sc.nextLine();
 
 
-    while(running){
-        System.out.println("======== MENU ========\n" +
-                "1 - Cadastrar produto\n" +
-                "2 - Listar produtos\n" +
-                "3 - Atualizar quantidade\n" +
-                "4 - Atualizar preço\n" +
-                "5 - Remover produto\n" +
-                "6 - Buscar produto\n" +
-                "7 - Sair\n" +
-                "Escolha uma opção:\n");
+                    Product p = new Product(nome, quantidade, preco);
+                    products.add(p);
 
-        int option;
-        option=sc.nextInt();
-        sc.nextLine();
-        switch (option){
-            case 1:
-                System.out.println("Digite o nome do produto");
-                String nome=sc.nextLine();
-                System.out.println("Digite o preço do produto ");
-                double preco = sc.nextDouble();
-                System.out.println("Digite a quantidade desse produto");
-                int quantidade=sc.nextInt();
-                sc.nextLine();//limpa a quebra
-                Product p = new Product(nome,preco,quantidade);//aqui eu criei o objeto Product
-                products.add(p);
-                System.out.println("Produto cadastrado com sucesso!");
+                    System.out.println("Produto cadastrado com sucesso!");
+                    break;
+
+                case 2:
+                    System.out.println("===LISTA DE PRODUTOS===");
+
+                    if (products.isEmpty()) {
+                        System.out.println("Nao há nenhum produto cadastrado até agora");
+                    } else {
+                        for (int i = 0; i < products.size(); i++) {
+                            Product item = products.get(i);
+
+                            System.out.println(
+                                    "Nome: " + item.getnome() +
+                                            " | Quantidade: " + item.getquantidade() +
+                                            " | Preço: " + item.getpreco()
+                            );
+                        }
+                    }
+
+                    break;
+                case 3:
+                    if (products.isEmpty()){
+                        System.out.println("Não há nenhum produto cadastrado até agora");
+
+                    }else{
+                        for (int i = 0; i <products.size() ; i++) {
+                            Product item = products.get(i);
+                            System.out.println("posição " + (i + 1) +
+                                    "Nome: " + item.getnome() +
+                                    " | Quantidade: " + item.getquantidade() +
+                                    " | Preço: " + item.getpreco());
+                        }
+                        System.out.println("Qual produto vc deseja alterar ?");
+                        int posicao=sc.nextInt();
+                        sc.nextLine();
+                        int index =posicao -1 ;
+                        Product escolhido= products.get(index);
+                        break;
+                        
 
 
 
+                        }
 
-
-                break;
-
-            case 2:
-                //Listas
-                break;
-
-            case 3:
-            // atualizar quantidade
-            break;
-
-            case 4:
-                // atualizar preço
-                break;
-
-            case 5:
-                // remover
-                break;
-
-            case 6:
-                // buscar
-                break;
-
-            case 7:
-             running = false;
-             break;
-
-            default:
-                //se digitar algo fora da lista
+                    }
+            }
         }
-    }
-    sc.close();
+
+        sc.close();
     }
 }
