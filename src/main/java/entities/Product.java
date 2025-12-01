7c87ce1db0b88e67769dd63b18b4aaa9d77f7e30
Cauan Pinho <Jobscauanpinho@gmail.com>
2025-12-01 13:54:47 -0300

    Adicionando Product.java completo

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,7 +1,26 @@
 package entities;
- import java.util.Scanner;
-public class  Product {
 
-        }
+public class Product {
+
+    private String nome;
+    private int quantidade;
+    private double preco;
+
+    public Product(String nome, int quantidade, double  preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
     }
 }
